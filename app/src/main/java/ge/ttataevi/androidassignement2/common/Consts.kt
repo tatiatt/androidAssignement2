54761e4449a7f2c12cbf9953db8124a044b50737
Vazhapp <vkentchiashvili@gmail.com>
2023-04-22 22:34:01 +0400

    implemented current city api source

---
+++ app/src/main/java/ge/ttataevi/androidassignement2/common/Consts.kt
@@ -0,0 +1,8 @@
+package ge.ttataevi.androidassignement2.common
+
+object Consts {
+    // Never try this at home
+    const val API_KEY = "60febec5b5c4593bd5befd8de5867f1e"
+
+    const val UNITS = "metric"
+}
