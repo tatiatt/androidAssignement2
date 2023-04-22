4967972fd0d1668f9cab7884cb00b328aececb5f
Vazhapp <vkentchiashvili@gmail.com>
2023-04-22 22:33:53 +0400

    implemented current city api source

--- app/src/main/java/ge/ttataevi/androidassignement2/data/source/WeatherSource.kt
+++ app/src/main/java/ge/ttataevi/androidassignement2/data/source/WeatherSource.kt
@@ -1,4 +1,15 @@
 package ge.ttataevi.androidassignement2.data.source
 
+import ge.ttataevi.androidassignement2.common.Consts.API_KEY
+import ge.ttataevi.androidassignement2.common.Consts.UNITS
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherSource {
+    @GET("data/2.5/weather?")
+    fun getCurrentCityWeather(
+        @Query("q") cityName: String,
+        @Query("appId") appId: String = API_KEY,
+        @Query("units") units: String = UNITS
+    )
 }
