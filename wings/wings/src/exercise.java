7272b1e326f02c0fbb818c586e3d36c722ca4089
Tatia.Tataevi <ttata19@freeuni.edu.ge>
2023-04-22 20:24:28 +0400

    Initial commit for android app

---
+++ wings/wings/src/exercise.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class exercise {
+
+
+        public static void main(String[] args) {
+
+            System.out.println(new Date().getYear());
+        }
+    }
+
+
+
